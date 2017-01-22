4814f79c96111a13766cf7f8f4cec84db2e4bb87
Michael McDuffee <mrmcduff@gmail.com>
2017-01-22 11:01:21 -0800

    now with custom filters

---
+++ mod2/ch12/app.js
@@ -0,0 +1,39 @@
+(function() {
+'use strict;'
+angular.module('CustomFilterModule', [])
+.controller('CustomFilterController', CustomFilterController)
+.filter('capital', CapitalizeFilterFactory);
+
+CustomFilterController.$inject = ['$scope', 'capitalFilter'];
+
+function CustomFilterController($scope, capitalFilter) {
+  $scope.userInput = "";
+  $scope.capitalize = function () {
+    $scope.userInput = capitalFilter($scope.userInput);
+  };
+}
+
+function CapitalizeFilterFactory() {
+  return function(input) {
+            input = input || "";
+            var wordArray = input.split(' ');
+            var outputString = "";
+            for (var i = 0; i < wordArray.length; i++) {
+              var word = wordArray[i].trim();
+              if (word == "") {
+                continue;
+              }
+              var remainder = word.length > 1 ? word.slice(1) : "";
+              word = word.charAt(0).toUpperCase() + remainder;
+              outputString += word;
+              outputString += ' ';
+            }
+
+            var changedInput = outputString.trim();
+            return changedInput;
+         };
+}
+
+console.log(CustomFilterController.toString());
+
+})();
