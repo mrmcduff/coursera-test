126e3fcd75140096e0bbc6d5fafa235a291a1178
Michael McDuffee <mrmcduff@stripe.com>
2017-01-26 18:00:11 -0800

    working solution

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -24,21 +24,45 @@ var initialList = [
   }
 ];
 
+
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
 function ToBuyController(ShoppingListCheckOffService) {
-  var toBuyController = this;
+  var buyList = this;
+
+  buyList.items = ShoppingListCheckOffService.getToBuyItems();
+
+  buyList.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItemAtIndex(itemIndex);
+  };
+}
+
+
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.items =
+    ShoppingListCheckOffService.getAlreadyBoughtItems();
 }
 
 function ShoppingListCheckOffService() {
   var service = this;
   var toBuyItems = initialList;
+  // var toBuyItems = initialList;
   // init purchasedItems as empty
   var alreadyBoughtItems = [];
+
+  service.buyItemAtIndex = function(itemIndex){
+    var item = toBuyItems[itemIndex];
+    toBuyItems.splice(itemIndex, 1);
+    service.buyItem(item.name, item.quantity);
+  };
+
   service.buyItem = function (itemName, quantity) {
     var item = {
       name: itemName,
