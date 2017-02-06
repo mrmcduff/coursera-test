0d899a2261851ef97fa6f33529f41e9dd683f343
Michael McDuffee <mrmcduff@gmail.com>
2017-02-05 16:30:05 -0800

    making the connection secure

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,7 +5,7 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
 .directive('foundItems', MenuSearchDirective)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 function MenuSearchDirective() {
   var ddo = {
